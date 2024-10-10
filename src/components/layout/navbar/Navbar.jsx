f7c9752e302d34631e2a011c454b6917f614975f
gian paolo <chesanipaolo@gmail.com>
2024-10-10 20:00:17 -0300

    listo para entregar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import "./Navbar.css";
 import {CartWidget} from "../../common/cartWidget/CartWidget"
+import { List, ListItem, ListItemText } from '@mui/material';
+
 
 export const Navbar =()=>{
     return(
@@ -7,13 +9,28 @@ export const Navbar =()=>{
         <div className="container-navbar">
 
             <img src="https://res.cloudinary.com/dlf2ifmnk/image/upload/v1728584278/Serene_logo_React_emtwac.png" alt="logo de Serene" />
+            
+            <List style={{ display: 'flex', padding: 0, margin: 0 }}>
+                <ListItem button>
+                    <ListItemText primary="Todas" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary="Bags" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary="MiniBags" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary="Accesorios" />
+                </ListItem>
+            </List>
 
-            <ul>
+            {/* <ul>
                 <li>Todas</li>
                 <li>Bags</li>
                 <li>MiniBags</li>
                 <li>Accesorios</li>
-            </ul>
+            </ul> */}
 
             <CartWidget/>
 
