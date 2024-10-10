3df553e4bbe5c6696518be73323a0a0ad63cfc5a
gian paolo <chesanipaolo@gmail.com>
2024-10-10 04:14:56 -0300

    hasta min 7 clase 4 - 3 bloques navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <>
     <h1>Limpieza del App.jsx</h1>
+    
     </>
   )
 }
---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+    return(
+        <>
+        <div className="container-navbar">
+
+            <h2>Serene</h2>
+
+            <ul>
+                <li>Todas</li>
+                <li>Bags</li>
+                <li>MiniBags</li>
+                <li>Accesorios</li>
+            </ul>
+
+            <h2>Icono de un carrito de compras</h2>
+
+        </div>
+        </>
+    );
+}
